bc3d9b9c7089b1c5cc34f431edff90ca07f91829
bahari49 <baharihari49@gmail.com>
2023-10-29 15:56:47 +0700

    perbaikan pada methode delete

--- resources/views/user/profile/layouts/deleteImageModal.blade.php
+++ resources/views/user/profile/layouts/deleteImageModal.blade.php
@@ -16,7 +16,7 @@
                 </form>
                 <form class="" method="POST" action="/delete_image/?image={{$user->foto}}">
                     @csrf
-                    @method('delete')
+                    @method('DELETE')
                     <button type="submit" class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
                         Yes, I'm sure
                     </button>
