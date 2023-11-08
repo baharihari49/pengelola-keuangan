73ffcb30537f08d3f33542a495c35c4d7c4dcb0f
bahari49 <baharihari49@gmail.com>
2023-11-08 14:06:39 +0700

    perbaikan pada bagian feedback

--- resources/views/dashboard/feedback/layouts/form_feedback.blade.php
+++ resources/views/dashboard/feedback/layouts/form_feedback.blade.php
@@ -8,9 +8,9 @@
                     <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori Feedback</label>
                     <select name="kategori" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         <option selected="">Select Kategori</option>
-                        <option value="TV">Error Found</option>
-                        <option value="PC">Improvement</option>
-                        <option value="GA">Tambah Fitur Baru</option>
+                        <option value="Error Found">Error Found</option>
+                        <option value="Improvement">Improvement</option>
+                        <option value="Tambah Fitur Baru">Tambah Fitur Baru</option>
                     </select>
                 </div>
                 <div class="sm:col-span-2">
