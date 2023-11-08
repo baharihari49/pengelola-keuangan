7b432efa2bfc54b84a45397d585ddc6bef7364ef
bahari49 <baharihari49@gmail.com>
2023-11-08 15:01:27 +0700

    perbaikan pada transaksi

--- resources/views/dashboard/transaksi/layouts/tabel_transaksi.blade.php
+++ resources/views/dashboard/transaksi/layouts/tabel_transaksi.blade.php
@@ -148,7 +148,7 @@
                             <select id="select-transaksi"
                                 class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                                 <option selected value="all">Semua</option>
-                                <option value="1">Pendapatan</option>
+                                <option value="1">Pemasukan</option>
                                 <option value="2">Pengeluaran</option>
                                 <option value="3">Tabungan</option>
 
--- resources/views/dashboard/transaksi/layouts/update_transaksi.blade.php
+++ resources/views/dashboard/transaksi/layouts/update_transaksi.blade.php
@@ -33,7 +33,7 @@
                     <div>
                         <label for="jenis_transaksi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Transaksi</label>
                         <select name="jenis_transaksi_id" id="jenis_transaks_update" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option id="opt-jenis-transaksi" value="1">Pendapatan</option>
+                            <option id="opt-jenis-transaksi" value="1">Pemasukan</option>
                             <option id="opt-jenis-transaksi" value="2">Pengeluaran</option>
                             <option id="opt-jenis-transaksi" value="3">Tabungan</option>
                         </select>
