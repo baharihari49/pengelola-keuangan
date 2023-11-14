ed502e7423d8f2a8dbac9081c20f324f1819aa0f
bahari49 <baharihari49@gmail.com>
2023-11-14 15:44:48 +0700

    perbaikan info bisnis

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -102,7 +102,7 @@
                         <form id="" method="POST" action="/delete_logo">
                             @csrf
                             @method('DELETE')
-                            <input type="hidden" name="logo" value="{{ $info_bisnis[0]->logo }}">
+                            <input type="hidden" name="logo" value="{{ isset($info_bisnis[0]) ? $info_bisnis[0]->logo : ''}}">
                             <button type="submit" class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
                                 Yes, I'm sure
                             </button>
