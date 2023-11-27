40035f5d0f55e7707440a014662b09b8ad22c6e5
bahari <baharihari49@gmail.com>
2023-11-27 15:56:57 +0700

    perbaikan pada fitur payment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
         //          ->dailyAt('23:16');
 
         $schedule->command('app:reset-payment-status')
-                 ->dailyAt('15:45');
+                 ->dailyAt('15:58');
     }
 
 
