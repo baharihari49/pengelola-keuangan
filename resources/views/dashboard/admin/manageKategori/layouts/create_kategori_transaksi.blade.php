f07a2df42d4f8aa1bbae5429602c75f4093a39a1
bahari <baharihari49@gmail.com>
2023-12-09 02:41:47 +0700

    add fitur tambah kategori by super admin

--- resources/views/dashboard/admin/manageKategori/layouts/create_kategori_transaksi.blade.php
+++ resources/views/dashboard/admin/manageKategori/layouts/create_kategori_transaksi.blade.php
@@ -36,9 +36,9 @@
                     <div>
                         <label for="jenis_transaksi"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Transaksi</label>
-                        <select name="jenis_transaksi_id" id="jenis_transaksi"
+                        <select required name="jenis_transaksi_id" id="jenis_transaksi"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected="">Select category</option>
+                            {{-- <option selected="">Select category</option> --}}
                             @foreach ($jenis_transaksi as $jt)
                             <option value="{{$jt->id}}">{{$jt->nama}}</option>
                             @endforeach
--- resources/views/dashboard/kategori/layouts/create_kategori_transaksi.blade.php
+++ resources/views/dashboard/kategori/layouts/create_kategori_transaksi.blade.php
@@ -32,13 +32,13 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             required="">
                     </div>
-                    
+
                     <div>
                         <label for="jenis_transaksi"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Transaksi</label>
-                        <select name="jenis_transaksi_id" id="jenis_transaksi"
+                        <select required name="jenis_transaksi_id" id="jenis_transaksi"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected="">Select category</option>
+                            {{-- <option selected="">Select category</option> --}}
                             @foreach ($jenis_transaksi as $jt)
                             <option value="{{$jt->id}}">{{$jt->nama}}</option>
                             @endforeach
@@ -65,7 +65,7 @@
 
     const jenis_transaksi = document.getElementById('jenis_transaksi')
     const kategori = document.getElementById('kategori')
-    
+
     jenis_transaksi.addEventListener('click', function() {
         kategori.disabled = false
 
