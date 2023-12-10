a297ee5ab8c235c1486df045110d285f72060370
Bintang Tobing <hello@bintangtobing.com>
2023-12-10 19:41:35 +0700

    update: top pengeluaran

--- resources/views/dashboard/top_pengeluaran.blade.php
+++ resources/views/dashboard/top_pengeluaran.blade.php
@@ -6,7 +6,6 @@
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-4 py-3 text-sm">no</th>
                             <th scope="col" class="px-4 py-3 text-sm">Kategori</th>
                             <th scope="col" class="px-4 py-3 text-sm">jumlah</th>
                         </tr>
@@ -16,7 +15,7 @@
                             <tr id="tabel-row" class="border-b dark:border-gray-700">
                                 <td class="px-4 py-3 text-red-600 font-medium">{{ $index + 1 }}</td>
                                 <td class="px-4 py-3 text-red-600 font-medium">
-                                    {{ substr($item['nama'], 0, 20) }}
+                                    {{ substr($item['nama'], 0, 15) }}
                                 </td>
                                 <td class="px-4 py-3 text-red-600 font-medium">Rp
                                     {{ number_format($item['total_jumlah'], 0, ',', '.') }}</td>
