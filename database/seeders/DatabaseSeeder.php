8701267cfb17fb74ad46c4346bd1706fd64a50d3
Bintang Tobing <hello@bintangtobing.com>
2023-12-10 12:34:15 +0700

    fix: databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,16 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            addSubJenisTransaksi::class,
-            addUserIdAtPayment::class,
-            AnggaranSeeder::class,
-            DashboardSeeder::class,
-            FeedbackCenterSeeder::class,
             Jenis_transaksisSeeder::class,
-            KategoriAnggaranSeeder::class,
             KategoriTransaksiSeeder::class,
             SUP_COS::class,
-            TransaksiSeeder::class,
             rolePermision::class,
             addPermsission::class,
             addRole::class,
